532d15bb69ae4218437b73ef4ff164af8b84e6c6
A. Sayef Reyadh <sayef.reyadh.aust@gmail.com>
2020-12-29 00:41:29 +0600

    upload csv to s3

--- Automate File Handling/automatic_s3_uploader.py
+++ Automate File Handling/automatic_s3_uploader.py
@@ -4,9 +4,13 @@ import os
 
 client = boto3.client('s3')
 
+upload_file_bucket = 'csv-dynamo-lambda-sayefreyadh'  # A dummy s3 bucket
 
 for file in os.listdir():
     if '.py' in file:
-        upload_file_bucket = 'dummy-bucket-sayefreyadh' #A dummy s3 bucket
-        upload_file_key = 'python/' + str(file) #A sub dir to store py files
+        upload_file_key = 'python/' + str(file)  # A sub dir to store py files
+        client.upload_file(file, upload_file_bucket, upload_file_key)
+
+    elif '.csv' in file:
+        upload_file_key = str(file)
         client.upload_file(file , upload_file_bucket, upload_file_key)
